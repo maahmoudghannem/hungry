fbf7d1d313a6fa5183854fc1f3296261c6449ea7
Mahmoud Ghanem <maahmoudghannem@gmail.com>
2025-10-02 15:39:32 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Hungry());
+
+class Hungry extends StatelessWidget {
+  const Hungry({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
